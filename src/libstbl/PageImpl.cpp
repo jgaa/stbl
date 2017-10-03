f92dcc7ae9965c8a91a73b3ff5e5e2d5aeb64a02
Jarle Aase <jarle@jgaa.com>
2017-10-03 17:22:32 +0300

    Hack to deal with code-blocks

--- src/libstbl/PageImpl.cpp
+++ src/libstbl/PageImpl.cpp
@@ -2,6 +2,8 @@
 #include <string.h>
 #include <fstream>
 
+#include <boost/algorithm/string/replace.hpp>
+
 #include "stbl/stbl.h"
 #include "stbl/Page.h"
 #include "stbl/logging.h"
@@ -24,9 +26,6 @@ public:
     }
 
     void Render2Html(std::ostream & out) override {
-
-        // TODO: Generate from template so that we get a full page with navigation
-
         ifstream in(path_.string());
         if (!in) {
             auto err = strerror(errno);
@@ -37,9 +36,60 @@ public:
         }
 
         EatHeader(in);
+        string content((std::istreambuf_iterator<char>(in)),
+                       istreambuf_iterator<char>());
 
-        markdown::Document doc(in);
-        doc.write(out);
+        // Quick hack to extract code blocks and convert them to a <pre> block
+        size_t pos = 0;
+        while((pos != content.npos) && (pos < content.size())) {
+            pos = content.find("```", pos);
+            if ((pos != content.npos) && (content.size() >= (pos + 7))) {
+                if ((pos > 0) && content.at(pos -1) != '\n') {
+                    // start-tag is only valid at start of line
+                    pos = content.find('\n', pos);
+                    continue;
+                }
+                const auto spos = content.find('\n', pos);
+                const auto epos = content.find("\n```\n", pos + 4);
+                if (epos != content.npos) {
+                    // pos = start ```
+                    // spos = end of start-line
+                    // epos is start of end ```
+
+                    //boost::replace_all(code_block, "&", "&amp;");
+
+                    string code = content.substr(spos, epos - spos);
+                    boost::replace_all(code, "<", "&lt;");
+                    boost::replace_all(code, ">", "&gt;");
+
+                    string code_block = "\n"s
+                        + R"(<pre class="code">)"s
+                        + code
+                        + "\n</pre>\n"s;
+
+                    content.replace(pos, (epos + 4) - pos, code_block);
+                }
+            } else {
+                break;
+            }
+        }
+
+        istringstream stream_content(content);
+        markdown::Document doc(stream_content);
+
+        stringstream out_stream;
+        doc.write(out_stream);
+
+        content.assign(istreambuf_iterator<char>(out_stream),
+                        istreambuf_iterator<char>());
+
+        // We need to fix some potential problems in code-blocks, where the
+        // the markdown process may escape &lt; and &gt;
+        // TODO: Fix it in the markdown processor, as this hack may have side-effects
+        boost::replace_all(content, "&amp;lt;", "&lt;");
+        boost::replace_all(content, "&amp;gt;", "&gt;");
+
+        out << content;
     }
 
 private:
