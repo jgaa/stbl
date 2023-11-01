74756257ba6518956e9ee27943859bcc875ebacd
Jarle Aase <jgaa@jgaa.com>
2023-11-01 21:55:45 +0200

    Removed test. It had no purtpse, and the test framework slowed down the build
    Removed boost-filesystem

--- cmake_scripts/external-projects.cmake
+++ cmake_scripts/external-projects.cmake
@@ -4,22 +4,22 @@
 # add_dependencies(TARGET externalProjectName)
 # target_link_libraries(TARGET PRIVATE ExternalLibraryName)
 
-set(EXTERNAL_PROJECTS_PREFIX ${CMAKE_BINARY_DIR}/external-projects)
-set(EXTERNAL_PROJECTS_INSTALL_PREFIX ${EXTERNAL_PROJECTS_PREFIX}/installed)
+#set(EXTERNAL_PROJECTS_PREFIX ${CMAKE_BINARY_DIR}/external-projects)
+#set(EXTERNAL_PROJECTS_INSTALL_PREFIX ${EXTERNAL_PROJECTS_PREFIX}/installed)
 
-include(GNUInstallDirs)
+#include(GNUInstallDirs)
 
-# MUST be called before any add_executable() # https://stackoverflow.com/a/40554704/8766845
-link_directories(${EXTERNAL_PROJECTS_INSTALL_PREFIX}/${CMAKE_INSTALL_LIBDIR})
-include_directories($<BUILD_INTERFACE:${EXTERNAL_PROJECTS_INSTALL_PREFIX}/${CMAKE_INSTALL_INCLUDEDIR}>)
+## MUST be called before any add_executable() # https://stackoverflow.com/a/40554704/8766845
+#link_directories(${EXTERNAL_PROJECTS_INSTALL_PREFIX}/${CMAKE_INSTALL_LIBDIR})
+#include_directories($<BUILD_INTERFACE:${EXTERNAL_PROJECTS_INSTALL_PREFIX}/${CMAKE_INSTALL_INCLUDEDIR}>)
 
-include(ExternalProject)
+#include(ExternalProject)
 
-ExternalProject_Add(externalLest
-    PREFIX "${EXTERNAL_PROJECTS_PREFIX}"
-    GIT_REPOSITORY "https://github.com/martinmoene/lest.git"
-    GIT_TAG "master"
-    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${EXTERNAL_PROJECTS_INSTALL_PREFIX} -DLEST_BUILD_EXAMPLE=OFF
-    TEST_BEFORE_INSTALL 0
-    TEST_AFTER_INSTALL 0
-    )
+#ExternalProject_Add(externalLest
+#    PREFIX "${EXTERNAL_PROJECTS_PREFIX}"
+#    GIT_REPOSITORY "https://github.com/martinmoene/lest.git"
+#    GIT_TAG "master"
+#    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${EXTERNAL_PROJECTS_INSTALL_PREFIX} -DLEST_BUILD_EXAMPLE=OFF
+#    TEST_BEFORE_INSTALL 0
+#    TEST_AFTER_INSTALL 0
+#    )
